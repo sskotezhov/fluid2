25da9c97ee9501264d52e198a4c874ea0e65020c
sskotezhov <sskotezhov@edu.hse.ru>
2024-12-21 23:23:44 +0300

    up

---
+++ 1.cpp
@@ -0,0 +1,515 @@
+#include <vector>
+#include <iostream>
+#include <chrono>
+#include <cassert>
+#include <ranges>
+#include <random>
+#include <algorithm>
+#include <thread>
+#include <condition_variable>
+#include <queue>
+#include <functional>
+#include <future>
+
+using namespace std;
+
+constexpr size_t N = 36, M = 84;
+// constexpr size_t N = 14, M = 5;
+constexpr size_t T = 1'000;
+constexpr std::array<pair<int, int>, 4> deltas{{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}};
+
+// char field[N][M + 1] = {
+//     "#####",
+//     "#.  #",
+//     "#.# #",
+//     "#.# #",
+//     "#.# #",
+//     "#.# #",
+//     "#.# #",
+//     "#.# #",
+//     "#...#",
+//     "#####",
+//     "#   #",
+//     "#   #",
+//     "#   #",
+//     "#####",
+// };
+
+char field[N][M + 1] = {
+    "####################################################################################",
+    "#                                                                                  #",
+    "#                                                                                  #",
+    "#                                                                                  #",
+    "#                                                                                  #",
+    "#                                                                                  #",
+    "#                                       .........                                  #",
+    "#..............#            #           .........                                  #",
+    "#..............#            #           .........                                  #",
+    "#..............#            #           .........                                  #",
+    "#..............#            #                                                      #",
+    "#..............#            #                                                      #",
+    "#..............#            #                                                      #",
+    "#..............#            #                                                      #",
+    "#..............#............#                                                      #",
+    "#..............#............#                                                      #",
+    "#..............#............#                                                      #",
+    "#..............#............#                                                      #",
+    "#..............#............#                                                      #",
+    "#..............#............#                                                      #",
+    "#..............#............#                                                      #",
+    "#..............#............#                                                      #",
+    "#..............#............################                     #                 #",
+    "#...........................#....................................#                 #",
+    "#...........................#....................................#                 #",
+    "#...........................#....................................#                 #",
+    "##################################################################                 #",
+    "#                                                                                  #",
+    "#                                                                                  #",
+    "#                                                                                  #",
+    "#                                                                                  #",
+    "#                                                                                  #",
+    "#                                                                                  #",
+    "#                                                                                  #",
+    "#                                                                                  #",
+    "####################################################################################",
+};
+class ThreadPool {
+private:
+    std::vector<std::thread> workers;
+    std::mutex mutex;
+    std::condition_variable cv;
+    std::queue<std::function<void()>> queue;
+    void worker();
+    bool stop;
+
+public:
+    ThreadPool(std::size_t nr_threads = std::thread::hardware_concurrency());
+    ~ThreadPool();
+
+    template<typename F, typename... Args>
+    auto enqueue(F&& f, Args&&... args) -> std::future<decltype(f(args...))>;
+
+    ThreadPool(ThreadPool&) = delete;
+    ThreadPool(const ThreadPool&) = delete;
+    ThreadPool& operator=(ThreadPool&&) = delete;
+    ThreadPool& operator=(const ThreadPool&) = delete;
+};
+ThreadPool::ThreadPool(std::size_t nr_workers) {
+    stop = false;
+    for (auto i{ 0 }; i < nr_workers; i++) {
+        workers.emplace_back(&ThreadPool::worker, this);
+    }
+}
+void ThreadPool::worker() {
+    for (;;) {
+        std::function<void()> cur_task;
+        {
+            std::unique_lock<std::mutex> lock(mutex);
+            cv.wait(lock, [this]() {
+                return stop || !queue.empty();
+            });
+
+            if (stop && queue.empty()) 
+                break;
+            if (queue.empty())
+                continue; 
+
+            cur_task = queue.front();
+            queue.pop();
+            // grab the fx from queue
+        }
+        cur_task();
+    }
+} 
+template<typename F, typename... Args>
+inline auto ThreadPool::enqueue(F&& f, Args&&... args)
+-> std::future<decltype(f(args...))> {
+    auto func = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
+    auto encapsulated_ptr =
+        std::make_shared<std::packaged_task<decltype(f(args...))()>>
+        (func);
+
+    std::future<std::result_of_t<F(Args...)>> future_object = encapsulated_ptr->get_future();
+    {
+        std::unique_lock<std::mutex> lock(mutex);
+        queue.emplace([encapsulated_ptr]() {
+            (*encapsulated_ptr)(); // execute the fx
+            });
+    }
+    cv.notify_one();
+    return future_object;
+}
+ThreadPool::~ThreadPool() {
+    {
+        std::unique_lock<std::mutex> lock(mutex);
+        stop = true;
+    }
+
+    cv.notify_all();
+    for (auto& worker : workers) {
+        worker.join();
+    }
+}
+
+
+struct Fixed {
+    constexpr Fixed(int v): v(v << 16) {}
+    constexpr Fixed(float f): v(f * (1 << 16)) {}
+    constexpr Fixed(double f): v(f * (1 << 16)) {}
+    constexpr Fixed(): v(0) {}
+
+    static constexpr Fixed from_raw(int32_t x) {
+        Fixed ret;
+        ret.v = x;
+        return ret;
+    } 
+
+    int32_t v;
+
+    auto operator<=>(const Fixed&) const = default;
+    bool operator==(const Fixed&) const = default;
+};
+
+static constexpr Fixed inf = Fixed::from_raw(std::numeric_limits<int32_t>::max());
+static constexpr Fixed eps = Fixed::from_raw(deltas.size());
+
+Fixed operator+(Fixed a, Fixed b) {
+    return Fixed::from_raw(a.v + b.v);
+}
+
+Fixed operator-(Fixed a, Fixed b) {
+    return Fixed::from_raw(a.v - b.v);
+}
+
+Fixed operator*(Fixed a, Fixed b) {
+    return Fixed::from_raw(((int64_t) a.v * b.v) >> 16);
+}
+
+Fixed operator/(Fixed a, Fixed b) {
+    return Fixed::from_raw(((int64_t) a.v << 16) / b.v);
+}
+
+Fixed &operator+=(Fixed &a, Fixed b) {
+    return a = a + b;
+}
+
+Fixed &operator-=(Fixed &a, Fixed b) {
+    return a = a - b;
+}
+
+Fixed &operator*=(Fixed &a, Fixed b) {
+    return a = a * b;
+}
+
+Fixed &operator/=(Fixed &a, Fixed b) {
+    return a = a / b;
+}
+
+Fixed operator-(Fixed x) {
+    return Fixed::from_raw(-x.v);
+}
+
+Fixed abs(Fixed x) {
+    if (x.v < 0) {
+        x.v = -x.v;
+    }
+    return x;
+}
+
+ostream &operator<<(ostream &out, Fixed x) {
+    return out << x.v / (double) (1 << 16);
+}
+
+Fixed rho[256];
+
+Fixed p[N][M]{}, old_p[N][M];
+
+struct VectorField {
+    array<Fixed, deltas.size()> v[N][M];
+    Fixed &add(int x, int y, int dx, int dy, Fixed dv) {
+        return get(x, y, dx, dy) += dv;
+    }
+
+    Fixed &get(int x, int y, int dx, int dy) {
+        size_t i = ranges::find(deltas, pair(dx, dy)) - deltas.begin();
+        assert(i < deltas.size());
+        return v[x][y][i];
+    }
+};
+
+VectorField velocity{}, velocity_flow{};
+int last_use[N][M]{};
+int UT = 0;
+
+
+mt19937 rnd(1337);
+
+tuple<Fixed, bool, pair<int, int>> propagate_flow(int x, int y, Fixed lim) {
+    last_use[x][y] = UT - 1;
+    Fixed ret = 0;
+    for (auto [dx, dy] : deltas) {
+        int nx = x + dx, ny = y + dy;
+        if (field[nx][ny] != '#' && last_use[nx][ny] < UT) {
+            auto cap = velocity.get(x, y, dx, dy);
+            auto flow = velocity_flow.get(x, y, dx, dy);
+            if (flow == cap) {
+                continue;
+            }
+            // assert(v >= velocity_flow.get(x, y, dx, dy));
+            auto vp = min(lim, cap - flow);
+                    auto st = std::chrono::steady_clock::now();
+            if (last_use[nx][ny] == UT - 1) {
+                velocity_flow.add(x, y, dx, dy, vp);
+                last_use[x][y] = UT;
+                // cerr << x << " " << y << " -> " << nx << " " << ny << " " << vp << " / " << lim << "\n";
+                return {vp, 1, {nx, ny}};
+            }
+                auto stop   = std::chrono::steady_clock::now();
+                auto cnt = std::chrono::duration_cast<std::chrono::milliseconds>(stop - st);
+                std::cout << "The time: " << cnt.count() << " ms\n"; //16086 ms without optimize
+            auto [t, prop, end] = propagate_flow(nx, ny, vp);
+            ret += t;
+            if (prop) {
+                velocity_flow.add(x, y, dx, dy, t);
+                last_use[x][y] = UT;
+                // cerr << x << " " << y << " -> " << nx << " " << ny << " " << t << " / " << lim << "\n";
+                return {t, prop && end != pair(x, y), end};
+            }
+        }
+    }
+    last_use[x][y] = UT;
+    return {ret, 0, {0, 0}};
+}
+
+Fixed random01() {
+    return Fixed::from_raw((rnd() & ((1 << 16) - 1)));
+}
+
+void propagate_stop(int x, int y, bool force = false) {
+    if (!force) {
+        bool stop = true;
+        for (auto [dx, dy] : deltas) {
+            int nx = x + dx, ny = y + dy;
+            if (field[nx][ny] != '#' && last_use[nx][ny] < UT - 1 && velocity.get(x, y, dx, dy) > 0) {
+                stop = false;
+                break;
+            }
+        }
+        if (!stop) {
+            return;
+        }
+    }
+    last_use[x][y] = UT;
+    for (auto [dx, dy] : deltas) {
+        int nx = x + dx, ny = y + dy;
+        if (field[nx][ny] == '#' || last_use[nx][ny] == UT || velocity.get(x, y, dx, dy) > 0) {
+            continue;
+        }
+        propagate_stop(nx, ny);
+    }
+}
+
+Fixed move_prob(int x, int y) {
+    Fixed sum = 0;
+    for (size_t i = 0; i < deltas.size(); ++i) {
+        auto [dx, dy] = deltas[i];
+        int nx = x + dx, ny = y + dy;
+        if (field[nx][ny] == '#' || last_use[nx][ny] == UT) {
+            continue;
+        }
+        auto v = velocity.get(x, y, dx, dy);
+        if (v < 0) {
+            continue;
+        }
+        sum += v;
+    }
+    return sum;
+}
+
+struct ParticleParams {
+    char type;
+    Fixed cur_p;
+    array<Fixed, deltas.size()> v;
+
+    void swap_with(int x, int y) {
+        swap(field[x][y], type);
+        swap(p[x][y], cur_p);
+        swap(velocity.v[x][y], v);
+    }
+};
+
+bool propagate_move(int x, int y, bool is_first) {
+    last_use[x][y] = UT - is_first;
+    bool ret = false;
+    int nx = -1, ny = -1;
+    do {
+        std::array<Fixed, deltas.size()> tres;
+        Fixed sum = 0;
+        for (size_t i = 0; i < deltas.size(); ++i) {
+            auto [dx, dy] = deltas[i];
+            int nx = x + dx, ny = y + dy;
+            if (field[nx][ny] == '#' || last_use[nx][ny] == UT) {
+                tres[i] = sum;
+                continue;
+            }
+            auto v = velocity.get(x, y, dx, dy);
+            if (v < 0) {
+                tres[i] = sum;
+                continue;
+            }
+            sum += v;
+            tres[i] = sum;
+        }
+
+        if (sum == 0) {
+            break;
+        }
+
+        Fixed p = random01() * sum;
+        size_t d = std::ranges::upper_bound(tres, p) - tres.begin();
+
+        auto [dx, dy] = deltas[d];
+        nx = x + dx;
+        ny = y + dy;
+        assert(velocity.get(x, y, dx, dy) > 0 && field[nx][ny] != '#' && last_use[nx][ny] < UT);
+
+        ret = (last_use[nx][ny] == UT - 1 || propagate_move(nx, ny, false));
+    } while (!ret);
+    last_use[x][y] = UT;
+    for (size_t i = 0; i < deltas.size(); ++i) {
+        auto [dx, dy] = deltas[i];
+        int nx = x + dx, ny = y + dy;
+        if (field[nx][ny] != '#' && last_use[nx][ny] < UT - 1 && velocity.get(x, y, dx, dy) < 0) {
+            propagate_stop(nx, ny);
+        }
+    }
+    if (ret) {
+        if (!is_first) {
+            ParticleParams pp{};
+            pp.swap_with(x, y);
+            pp.swap_with(nx, ny);
+            pp.swap_with(x, y);
+        }
+    }
+    return ret;
+}
+
+int dirs[N][M]{};
+int main() {
+    ThreadPool tr(4);
+    rho[' '] = 0.01;
+    rho['.'] = 1000;
+    Fixed g = 0.1;
+    for (size_t x = 0; x < N; ++x) {
+        for (size_t y = 0; y < M; ++y) {
+            if (field[x][y] == '#')
+                continue;
+            for (auto [dx, dy] : deltas) {
+                dirs[x][y] += (field[x + dx][y + dy] != '#');
+            }
+        }
+    }
+    std::vector<std::future<void>> results;
+    for (size_t i = 0; i < T; ++i) {
+        
+        Fixed total_delta_p = 0;
+        // Apply external forces
+        for (size_t x = 0; x < N; ++x) {
+            
+            for (size_t y = 0; y < M; ++y) {
+                if (field[x][y] == '#' || field[x + 1][y] == '#') continue;
+                    velocity.add(x, y, 1, 0, g);
+            }
+        }
+        // Apply forces from p
+        memcpy(old_p, p, sizeof(p));
+        for (size_t x = 0; x < N; ++x) {
+            for (size_t y = 0; y < M; ++y) {
+                if (field[x][y] == '#')
+                    continue;
+                for (auto [dx, dy] : deltas) {
+                    int nx = x + dx, ny = y + dy;
+                    if (field[nx][ny] != '#' && old_p[nx][ny] < old_p[x][y]) {
+                        auto delta_p = old_p[x][y] - old_p[nx][ny];
+                        auto force = delta_p;
+                        auto &contr = velocity.get(nx, ny, -dx, -dy);
+                        if (contr * rho[(int) field[nx][ny]] >= force) {
+                            contr -= force / rho[(int) field[nx][ny]];
+                            continue;
+                        }
+                        force -= contr * rho[(int) field[nx][ny]];
+                        contr = 0;
+                        velocity.add(x, y, dx, dy, force / rho[(int) field[x][y]]);
+                        p[x][y] -= force / dirs[x][y];
+                        total_delta_p -= force / dirs[x][y];
+                    }
+                }
+            }
+        }
+        // Make flow from velocities
+        velocity_flow = {};
+        bool prop = false;
+        do {
+            UT += 2;
+            prop = 0;
+            for (size_t x = 0; x < N; ++x) {
+                for (size_t y = 0; y < M; ++y) {
+                    if (field[x][y] != '#' && last_use[x][y] != UT) {
+                        auto [t, local_prop, _] = propagate_flow(x, y, 1);
+                        if (t > 0) {
+                            prop = 1;
+                        }
+                    }
+                }
+            }
+        } while (prop);
+        // Recalculate p with kinetic energy
+        for (size_t x = 0; x < N; ++x) {
+            for (size_t y = 0; y < M; ++y) {
+                if (field[x][y] == '#')
+                    continue;
+                for (auto [dx, dy] : deltas) {
+                    auto old_v = velocity.get(x, y, dx, dy);
+                    auto new_v = velocity_flow.get(x, y, dx, dy);
+                    if (old_v > 0) {
+                        assert(new_v <= old_v);
+                        velocity.get(x, y, dx, dy) = new_v;
+                        auto force = (old_v - new_v) * rho[(int) field[x][y]];
+                        if (field[x][y] == '.')
+                            force *= 0.8;
+                        if (field[x + dx][y + dy] == '#') {
+                            p[x][y] += force / dirs[x][y];
+                            total_delta_p += force / dirs[x][y];
+                        } else {
+                            p[x + dx][y + dy] += force / dirs[x + dx][y + dy];
+                            total_delta_p += force / dirs[x + dx][y + dy];
+                        }
+                    }
+                }
+            }
+        }
+
+
+        UT += 2;
+        prop = false;
+        for (size_t x = 0; x < N; ++x) {
+            for (size_t y = 0; y < M; ++y) {
+                if (field[x][y] != '#' && last_use[x][y] != UT) {
+                    if (random01() < move_prob(x, y)) {
+                        prop = true;
+                        propagate_move(x, y, true);
+                    } else {
+                        propagate_stop(x, y, true);
+                    }
+                }
+            }
+        }
+
+        if (prop) {
+            cout << "Tick " << i << ":\n";
+            for (size_t x = 0; x < N; ++x) {
+                cout << field[x] << "\n";
+            }
+        }
+    }
+}
